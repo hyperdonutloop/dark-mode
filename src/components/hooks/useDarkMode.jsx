43ac1cab02d8032326971e26b7153b3a9d4b9ccd
donutwizard666 <thebeardedwarrior@icloud.com>
2019-10-30 15:09:51 -0700

    dark mode toggle works

---
+++ src/components/hooks/useDarkMode.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useLocalStorage }  from "./useLocalStorage";
+
+function useDarkMode (key, initialValue) {
+  const [ value, setValue ] = useLocalStorage(key, initialValue);
+  const body = document.querySelector('body');
+  useEffect(() => {
+    if(value) {
+      body.classList.add('dark-mode');
+    } else {
+      body.classList.remove('dark-mode');
+    }
+  },[value])
+  return [value, setValue]
+}
+
+export default useDarkMode;
